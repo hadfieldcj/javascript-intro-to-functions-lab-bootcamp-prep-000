b1462a8f23f03090abce6b67d490623655fa073f
hadfieldcj@gmail.com <hadfieldcj@gmail.com>
2018-05-02 04:37:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function whisper(string) {
-  return string.toLowerCase
+  return string.toLowerCase()
 }
 
 function shout(string) {
@@ -7,18 +7,18 @@ function shout(string) {
   }
   
 function logShout(string) {
-  console.log(string.toUpperCase)
+  console.log(string.toUpperCase())
 }
 
 function logWhisper(sting) {
-  console.log(string.toLowerCase)
+  console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase === string) {
+  if (string.toLowerCase() === string) {
     return "I can\'t hear you!"
   }
-  else if (string.toUpperCase === string) {
+  else if (string.toUpperCase() === string) {
     return "YES INDEED!"
   }
   else if (string === "I love you, Grandma.") {
